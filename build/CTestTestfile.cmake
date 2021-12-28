0aa5d9f19687be71db4f4aacbcddbac98d91870d
JandersonLRibeiro <jandersonlucasribeiro@gmail.com>
2021-12-28 15:13:57 -0300

    Criando workspace movimento

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/janderson/movimento/movimento/src
+# Build directory: /home/janderson/movimento/movimento/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/janderson/movimento/movimento/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
